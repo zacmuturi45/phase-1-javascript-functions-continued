170e67aae55aca56a594e47b5b6ca5cc6b16d102
isaacgichuhi <zacmuturi45@gmail.com>
2023-09-24 14:58:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-// code your solution here
+function saturdayFun(message= 'roller-skate') {
+    return `This Saturday, I want to ${message}!`
+};
+saturdayFun();
+
+function mondayWork(message= "go to the office") {
+    return `This Monday, I will ${message}.`
+}
+mondayWork("work from home");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function wrapAdjective(msg = "*") {
+    const result = function (emphatic = "special") {
+      return `You are ${msg}${emphatic}${msg}!`;
+    };
+    return result;
+  }
+  
+
+console.log(wrapAdjective("||")("a dedicated programmer"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
